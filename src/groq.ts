8d05ffb87bfa095cfa216b35656289662b5f5cb6
ocfox <i@ocfox.me>
2025-02-08 14:29:23 +0800

    ?

--- src/groq.ts
+++ src/groq.ts
@@ -19,7 +19,7 @@ function initChat(prompt: string) {
 export function getGroqChatCompletion(messages: Message[]) {
   return groq.chat.completions.create({
     messages: messages,
-    model: "deepseek-r1-distill-llama-70b-specdec",
+    model: "deepseek-r1-distill-llama-70b",
   });
 }
 
